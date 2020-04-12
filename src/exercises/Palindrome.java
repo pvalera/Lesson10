fecbd32bd94a4bc3eae2c76c1943897bee0d612a
Joshua J <pvalera@hotmail.com>
2020-04-12 11:49:41 -0700

    Added code to test to check for palindrome

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -9,4 +9,23 @@ public class Palindrome {
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
 	}
+	
+	public boolean wordExists(String word) {
+		if (loadWords().contains(word)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isPalindrome(String word) {
+		String reversedString = "";
+		for (int i = word.length() - 1; i>= 0; i--) {
+			reversedString = reversedString + word.charAt(i);
+		}
+		
+		if (word.equals(reversedString)) {
+			return true;
+		}
+		return false;
+	}
 }
