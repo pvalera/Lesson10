bcf95a42433d6e6c7c60a1c9f16b0c58c97c2552
Joshua J <pvalera@hotmail.com>
2020-04-12 11:50:07 -0700

    Added code to decrypt message

--- src/exercises/Encryptor.java
+++ src/exercises/Encryptor.java
@@ -12,5 +12,15 @@ public class Encryptor {
 		}
 		return builder.toString();
 	}
-
+	
+	public static String decrypt(String decryptMe) {
+		
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < decryptMe.length(); i++) {
+			int numericalValue = decryptMe.charAt(i);
+			numericalValue-=2;
+			builder.append((char) numericalValue);
+		}
+		return builder.toString();
+	}
 }
