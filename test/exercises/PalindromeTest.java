a637e3fdbc7737e6ae55d2b2454bca06aa7eaee7
Joshua J <pvalera@hotmail.com>
2020-04-12 11:49:06 -0700

    Added unit tests to check for palindrome

--- test/exercises/PalindromeTest.java
+++ test/exercises/PalindromeTest.java
@@ -14,7 +14,13 @@ class PalindromeTest {
 	}
 	
 	// 2. Test that a word exists in the dictionary 
-	
+	@Test
+	void testWordExists() {
+		assertTrue(testPalindrome.wordExists("cuckoo"));
+	}
 	// 3. Test that a word is a palindrome
-
+	@Test
+	void testPalindrome() {
+		assertTrue(testPalindrome.isPalindrome("racecar"));
+	}
 }
